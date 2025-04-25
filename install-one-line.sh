093b86c62f0871ef39fc2b47fce47f8ba6a0e463
LuizBranco <luizbranco@clickhype.com.br>
2025-04-25 01:48:42 -0300

    Adicionar script one-line para instalação

---
+++ install-one-line.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Script de instalação em uma linha
+curl -fsSL https://raw.githubusercontent.com/LuizBranco-ClickHype/vpsmcp/main/instalador.sh -o instalador.sh && chmod +x instalador.sh && ./instalador.sh
